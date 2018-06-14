69f411fffa387c9bd83318c5c15abc341f6117cd
Talha Balaj <talhabalaj@hotmail.com>
2018-06-14 02:34:55 +0100

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: 'index.js',
+  entry: './src/index.js',
   output: {
-    filename: 'build/bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
