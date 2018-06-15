c54afbf5c7fd8686f0a48db092aa6069787e4c73
Talha Balaj <talhabalaj@hotmail.com>
2018-06-15 19:22:41 +0100

    minor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-import Client from 'utorrent-api';
-import { getType } from 'mime';
-
-const uTorrent = new Client('localhost', '7777');
-uTorrent.setCredentials('admin', 'admin');
+const result = fetch('http://localhost:7777/gui');
+console.log(result);
